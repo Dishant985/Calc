d0335e418c08b7d4224097bebfab8374d5fe4ee1
Dishant985 <dishantshridhar14@gmail.com>
2025-08-07 11:17:01 +0530

    addition done

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class add {
+    public static void main(String[] args) {
+        
+        java.util.Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num: ");
+        int n = sc.nextInt();
+        System.out.println("Enter second num: ");
+        int m = sc.nextInt();
+        System.out.println(n+m);
+    }
+}
